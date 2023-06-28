030a7c5b99c920abf89067f0c0cad67bdc367363
sj <sujung033131@gmail.com>
2023-06-28 16:02:58 +0900

    0628 Kiosk step2

--- src/components/Kiosk.js
+++ src/components/Kiosk.js
@@ -1,3 +1,4 @@
+import { useState } from "react"
 
 const products = [
     {pno: 1, pname:'Americano', price: 7000},
@@ -8,8 +9,24 @@ const products = [
 
 const Kiosk = () => {
 
+    const [items, setItems] = useState([])
+
     const handleClickBuy = (product) => {
-        console.log(product)
+        console.log({...product, qty:1})
+
+        const result = items.filter( ele => ele.pno === product.pno )
+
+        console.log("result", result)
+
+        if(result.length === 0){
+            setItems([...items, {...product, qty:1}])
+            return
+        }
+
+        const cartItem = result[0]
+        cartItem.qty += 1
+        setItems([...items])
+
     }
 
     return ( 
@@ -31,6 +48,22 @@ const Kiosk = () => {
             </div>
             <div className="w-1/3">
                 <div className="text-4xl font-extrabold">Cart</div> 
+
+                <ul>
+                    {items.map( (item,idx) => 
+                    <li key={idx} className="border-2">
+                        <div className="flex text-3xl text-white m-4 p-4">
+                            <div>{item.pno}</div>
+                            <div>{item.pname}</div>
+                            <div>{item.price}</div>
+                        </div>
+                        <div className="flex justify-center text-2xl">
+                            <button className="m-1 rounded-lg bg-orange-400 p-4">+</button>
+                            <p className="m-2 text-red-600 p-2">{item.qty}</p>
+                            <button className="m-1 rounded-lg bg-orange-400 p-4">-</button>
+                        </div>
+                    </li> )}
+                </ul>
             </div>
         </div>
      );
