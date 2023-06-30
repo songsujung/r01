cd0cd4b7b74d745446cb08f1c8a97ae9dd150731
sj <sujung033131@gmail.com>
2023-06-30 17:17:49 +0900

    0630 Todo1 - step2 : CRUD

--- src/components/todo1/Todo1Input.js
+++ src/components/todo1/Todo1Input.js
@@ -1,34 +1,37 @@
 import { useState } from "react";
 
-const Todo1Input = ({changeView, addTodo}) => {
+const Todo1Input = ({addTodo}) => {
 
-    const [todo, setTodo] = useState({title:''})
+  const [todo, setTodo] = useState({title: ''})
 
-    return ( 
-        <div className="w-full h-[90vh] bg-green-200">
-            <div>Todo1 Input</div>
-            <div>
-                <input
-                 className="m-3 p-3 bg-blue-300"
-                 type='text'
-                 name='title'
-                 value={todo.title}
-                 onChange={(e) => {
-                    todo.title = e.target.value
-                    setTodo ({...todo})
-                }} ></input>
-                <button 
-                className="bg-blue-300 m-3 p-3 border-2"
-                onClick={() => {
-                    addTodo(todo)
-                    setTodo({title:''})
-                }}
-                >
-                    ADD Todo
-                </button>
-            </div>
-        </div>
-     );
+  return (
+    <div className="w-full bg-green-300">
+      <div>Todo1 Input</div>
+      <div>
+        <input
+          className="m-3 p-3 bg-blue-300"
+          type="text"
+          name="title"
+          value={todo.title}
+          onChange={(e) => {
+              todo.title = e.target.value
+              setTodo({...todo})
+            }
+          }
+        />
+        {/* 클릭하면 index쪽으로 전달되어야 함 그래서 함수는 index에서 선언 */}
+        <button
+          className="bg-blue-300 m-3 p-3 border-2 border-black rounded-lg"
+          onClick={() => {
+            addTodo(todo)
+            setTodo({title: ''})
+          }}
+        >
+          ADD Todo
+        </button>
+      </div>
+    </div>
+  );
 }
- 
+
 export default Todo1Input;
--- src/components/todo1/Todo1Read.js
+++ src/components/todo1/Todo1Read.js
@@ -1,7 +1,40 @@
-const Todo1Read = ({changeView}) => {
+import { useEffect, useState } from "react";
+
+const Todo1Read = ({current, remove, modify}) => {
+
+    const [todo, setTodo] = useState(current)
+
+    useEffect(() => {
+        setTodo(current)
+    },[current])
+
+    if(!todo){
+        return <>NOT Available</>
+    }
+
     return ( 
-        <div className="w-full h-[90vh] bg-pink-300">
+        <div className="w-full bg-pink-300">
             <div>Todo1 Read</div>
+            <div>
+                {todo.tno}
+            </div>
+            <div>
+                <input 
+                type='text' 
+                name='title' 
+                value={todo.title} 
+                onChange={(e) =>{ 
+                    todo.title = e.target.value 
+                    setTodo({...todo})
+                }}/>
+            </div>
+            <div>
+                <button className="m-1 p-2 bg-blue-300"
+                onClick={() => modify(todo)}>MOD</button>
+                <button className="m-1 p-2 bg-red-600"
+                onClick={() => remove(todo.tno)}
+                >DEL</button>
+            </div>
         </div>
      );
 }
